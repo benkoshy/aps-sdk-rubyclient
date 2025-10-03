f07815fc7b1376f6a9898295e3807263a3fed8f2
Ben Koshy <benkoshy@hotmail.com>
2025-10-03 20:18:40 +1000

    initialise repository

---
+++ lib/aps/sdk/rubyclient.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "rubyclient/version"
+
+module Aps
+  module Sdk
+    module Rubyclient
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/aps/sdk/rubyclient/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Aps
+  module Sdk
+    module Rubyclient
+      VERSION = "0.1.0"
+    end
+  end
+end
