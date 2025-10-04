1793e42dfd1d65b5a65ae8c2b21bddd3f1ecc002
Ben Koshy <benkoshy@hotmail.com>
2025-10-04 19:50:37 +1000

    WIP: add authentication requests

--- test/rubyclient_test.rb
+++ test/rubyclient_test.rb
@@ -3,17 +3,21 @@
 require_relative "test_helper"
 
 class TestInitialize < MiniTest::Unit::TestCase
-
-
-
   def test_argument_error
     assert_raises ArgumentError do
       @ruby_client = Aps::Sdk::Rubyclient.new
     end
-  end
+  end  
+end
+
+class TestMethods < MiniTest::Unit::TestCase
 
   def test_base_url
     @ruby_client = Aps::Sdk::Rubyclient.new(client_id: 1, client_secret: "secret")
     assert_equal @ruby_client.base_url, "https://developer.api.autodesk.com/"
   end
+  
 end
+
+
+
